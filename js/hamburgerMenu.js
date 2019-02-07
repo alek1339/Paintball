3d83d75c631ea4589b3415fcbed8fa18c7c568fd
alek1339 <aleksandermihaylov@gmail.com>
2019-02-07 20:35:17 +0200

    99% done

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -1,27 +1,40 @@
 function myFunction() {
-    var x = document.getElementById("nav");
-    if (x.className === "notshowed") {
-        x.style.display = "block";
+    var nav = document.getElementById("nav");
+    let li = document.getElementsByTagName('li')
+
+    if (nav.className === "notshowed") {
+        nav.className = "showed";
+        nav.style.display = "block";
 
     } else {
-        x.className = "notshowed";
+        nav.className = "notshowed";
+        nav.style.display = "none";
+
     }
 }
 
 function resized(){
     var width = window.outerWidth;
-    var x = document.getElementById("nav");
+    var nav = document.getElementById("nav");
     let icon = document.getElementsByClassName('icon')
+    let li = document.getElementsByTagName('li')
 
     if(width > 875){
-        x.style.display = "block";
+        nav.style.display = "block";
         icon[0].style.display = 'none'
-        console.log('Over' + width)
+        
+        for(let i = 0; i < li.length; i++){
+            li[i].style.display = 'inline'
+        }
         
     } else {
-        x.style.display = "none";
+        nav.style.display = "none";
         icon[0].style.display = 'block'
-        console.log('Over' +  width)
+        
+        for(let i = 0; i < li.length; i++){
+            li[i].style.display = 'block'
+        }
+
     }
 
 }
