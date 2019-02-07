6f64de98f8080acccc8544404b228de1ac3e70b3
alek1339 <aleksandermihaylov@gmail.com>
2019-02-07 20:14:38 +0200

    99% done

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -2,22 +2,26 @@ function myFunction() {
     var x = document.getElementById("nav");
     if (x.className === "notshowed") {
         x.style.display = "block";
-        x.className = "showed";
 
     } else {
         x.className = "notshowed";
-        x.style.display = "none";
     }
 }
 
 function resized(){
     var width = window.outerWidth;
     var x = document.getElementById("nav");
-    if(width > 860){
-        x.style.display = "inline";
-    }
-    if(width <= 860){
+    let icon = document.getElementsByClassName('icon')
+
+    if(width > 875){
+        x.style.display = "block";
+        icon[0].style.display = 'none'
+        console.log('Over' + width)
+        
+    } else {
         x.style.display = "none";
+        icon[0].style.display = 'block'
+        console.log('Over' +  width)
     }
 
 }
